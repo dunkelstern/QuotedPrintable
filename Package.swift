8c2dc2a5852337713eb6af3433ff31bb3d266619
Johannes Schriewer <hallo@dunkelstern.de>
2015-12-20 23:09:28 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  QuotedPrintable
+//
+//  Created by Johannes Schriewer on 2015-12-20.
+//  Copyright © 2015 Johannes Schriewer. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "QuotedPrintable"
+)
